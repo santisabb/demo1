b14aacdabb43fe0bf6dfde92b2d1ef1da342cbf7
SANTIAGO SABBIONI <santisabbioni@gmail.com>
2023-07-27 18:09:46 -0300

    primera implementacion de base de datos JPA

--- src/main/java/com/example/demo1/Demo1Application.java
+++ src/main/java/com/example/demo1/Demo1Application.java
@@ -2,12 +2,19 @@ package com.example.demo1;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class Demo1Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Demo1Application.class, args);
+		ApplicationContext context = SpringApplication.run(Demo1Application.class, args);
+		UsuarioRepository repo = context.getBean(UsuarioRepository.class);
+		System.out.println("El numero de usuarios en la base de datos es: " + repo.count());
+		Usuario user1 = new Usuario(41312139, "Santi Sabbioni", "santisabb@gmail.com", 440934 , 0);
+		repo.save(user1);
+		System.out.println("El numero de usuarios en la base de datos es: " + repo.count());
+		System.out.println(repo.findAll());
 	}
 
 }
---
+++ src/main/java/com/example/demo1/Usuario.java
@@ -0,0 +1,79 @@
+package com.example.demo1;
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Usuario {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long nroDni;
+
+    private String nombreyApellido;
+    private String mail;
+    private Long nroTelefono;
+    private int cantReservas;
+
+    public Usuario(long nroDni, String nombreyApellido, String mail, long nroTelefono, int cantReservas) {
+        this.nroDni = nroDni;
+        this.nombreyApellido = nombreyApellido;
+        this.mail = mail;
+        this.nroTelefono = nroTelefono;
+        this.cantReservas = cantReservas;
+    }
+    public Usuario(){}
+    public long getNroDni() {
+        return nroDni;
+    }
+
+    public void setNroDni(long nroDni) {
+        this.nroDni = nroDni;
+    }
+
+    public String getNombreyApellido() {
+        return nombreyApellido;
+    }
+
+    public void setNombreyApellido(String nombreyApellido) {
+        this.nombreyApellido = nombreyApellido;
+    }
+
+    public String getMail() {
+        return mail;
+    }
+
+    public void setMail(String mail) {
+        this.mail = mail;
+    }
+
+    public long getNroTelefono() {
+        return nroTelefono;
+    }
+
+    public void setNroTelefono(long nroTelefono) {
+        this.nroTelefono = nroTelefono;
+    }
+
+    public int getCantReservas() {
+        return cantReservas;
+    }
+
+    public void setCantReservas(int cantReservas) {
+        this.cantReservas = cantReservas;
+    }
+
+
+    @Override
+    public String toString() {
+        return "Usuario{" +
+                "nroDni=" + nroDni +
+                ", nombreyApellido='" + nombreyApellido + '\'' +
+                ", mail='" + mail + '\'' +
+                ", nroTelefono=" + nroTelefono +
+                ", cantReservas=" + cantReservas +
+                '}';
+    }
+}
