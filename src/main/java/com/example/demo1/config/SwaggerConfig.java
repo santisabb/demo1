6ab235604dc0583b8efc5c30f7fdaa4084595fdb
SANTIAGO SABBIONI <santisabbioni@gmail.com>
2023-08-22 15:29:45 -0300

    agrego swagger-ui

--- src/main/java/com/example/demo1/config/SwaggerConfig.java
+++ src/main/java/com/example/demo1/config/SwaggerConfig.java
@@ -1,14 +1,13 @@
 package com.example.demo1.config;
 
+
+import io.swagger.v3.oas.models.ExternalDocumentation;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import springfox.documentation.builders.PathSelectors;
-import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.service.ApiInfo;
-import springfox.documentation.service.Contact;
-import springfox.documentation.spi.DocumentationType;
-import springfox.documentation.spring.web.plugins.Docket;
 
 import java.util.Collections;
 
@@ -16,24 +15,13 @@ import java.util.Collections;
 public class SwaggerConfig {
 
     @Bean
-    public Docket api(){
-        return new Docket(DocumentationType.SWAGGER_2)
-                .apiInfo(apiDetails())
-                .select()
-                .apis(RequestHandlerSelectors.any())
-                .paths(PathSelectors.any())
-                .build();
-    }
+    public OpenAPI demo1API(){
+        return new OpenAPI()
+            .info(new Info()
+                    .title("Demo1 API")
+              .description("Demo1 project by Santi")
+              .version("v0.0.1")
+              .license(new License().name("Apache 2.0").url("http://springdoc.org")));
 
-    private ApiInfo apiDetails() {
-
-        return new ApiInfo("First Springboot Api REST",
-                "Library Api REST docs",
-                "V1.0",
-                "https://www.google.com/" ,
-                new Contact("Santi" , "https://www.google.com/" , "santisabbioni@gmail.com") ,
-                "MIT" ,
-                "https://www.google.com/" ,
-                Collections.emptyList());
     }
 }
